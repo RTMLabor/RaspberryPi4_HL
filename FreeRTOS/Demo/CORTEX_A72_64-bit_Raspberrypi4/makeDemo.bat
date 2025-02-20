1cc11f8fb369f237b6c74472473794cb288a560d
Nelson <nelsonehling@posteo.de>
2025-02-20 08:53:05 +0100

    README erweitert, makeDemo.bat mit richtigem Compiler-Verzeichnis erweitert

--- FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/makeDemo.bat
+++ FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/makeDemo.bat
@@ -1,5 +1,5 @@
 
-SET PATH=%PATH%;C:\msys64\usr\bin;C:\camel-dev\D71\gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-elf\bin
+SET PATH=%PATH%;C:\msys64\usr\bin;C:\camel-dev\D71ADevRaspi\gcc-arm-10.3-2021.07-mingw-w64-i686-aarch64-none-elf\bin
 
 cd uart
 make CROSS=aarch64-none-elf- all
